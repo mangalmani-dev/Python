fa6cc5251f49b0451cfa0b876788e6e432c25a2d
mangalmani-dev <mangalmani2131@gmail.com>
2026-04-26 10:56:32 +0530

    completed with numbers in details

---
+++ 02_Numbers/basics.py
@@ -0,0 +1,67 @@
+# // Decarations and Assignments
+x =2
+y = 3
+# z = x + y
+# print(z)
+
+# # // diffrent numbers value 
+# a =2.5
+# b =3
+# print(a+b)
+# print((x+y)*z)
+
+# # // converting float to integer
+# print(int(a))
+
+# #  // Tuples concept
+# print(x,y,z)
+
+# #  // operators
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x%y)
+
+
+#  print the value in diffrent format
+# print(repr('chia'))
+# print(str('chia'))
+# print('chai')
+
+
+# #  comperator operators
+# print(x==y)
+# print(x!=y)
+# print(x>y)
+# print(x<y)
+# print (3==4)
+
+#  mathmatical functions
+# import math
+# print(math.sqrt(16))
+# print(math.pow(2,3))
+# print(math.pi)
+# c = 2.333
+# print(math.ceil(c))
+# print(math.trunc(c))
+
+
+# #  random numbers
+import random
+print(random.randint(1,10))
+l1 = ["apple","banana","cherry"]
+print(random.choice(l1))
+
+# // decimal numbers
+from decimal import Decimal
+a = Decimal('0.1')
+b = Decimal('0.2')  
+print(a+b)
+
+
+# setone
+
+setone = {1,2,3,4,5}
+print(setone & {2,3})
+print(setone | {6,7,8})
+
