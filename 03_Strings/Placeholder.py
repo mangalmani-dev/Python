4e78d435a87c78426c7aeae65a9cd3fd807da06b
mangalmani-dev <mangalmani2131@gmail.com>
2026-04-27 11:00:02 +0530

    completed with string

---
+++ 03_Strings/Placeholder.py
@@ -0,0 +1,23 @@
+# chai_type = "Masala chai"
+# Quantity = 2
+# Ordered = "I have Oredered {} cups of {}"
+# print(Ordered.format(Quantity, chai_type))
+
+# #  list to srings
+
+# chai_variety = ["Masala", "Ginger", "Green"]
+# print(", ".join(chai_variety))
+
+
+chai  = "Masala Chai"
+print(len(chai))   
+ 
+#   To print each letter in the string
+# for letter in chai:
+#     print(letter)
+
+
+chai  = "He said,\"masala chai is the best\""
+print(chai)
+
+print("Masala "in chai)
---
+++ 03_Strings/bascis.py
@@ -0,0 +1,39 @@
+#  some syntax related stuff in python
+
+chai = "masala chai chai"
+print(chai)
+
+
+#  some string functions
+# print(chai.upper())
+# print(chai.lower())
+# print(chai.title())
+# print(chai.strip())
+# print(chai.replace("masala", "ginger"))
+print(chai)
+print(chai.find("chai"))
+print(chai.count("chai"))
+
+
+
+# tea = "green tea, black tea, herbal tea"
+# print(tea.split(", "))
+
+
+
+
+
+# #  slcie operation
+# print(chai)
+# print(chai[0:6])
+# print(chai[-1])
+
+# list = "0123456789"
+# print(list)
+# print(list[5:])
+# print(list[3:7])
+# print(list[:8])
+# #  concept of hopping 
+# print(list[2:7:2]) 
+
+
