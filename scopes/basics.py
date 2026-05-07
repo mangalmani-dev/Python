2de017de58e4c4fceb6ed1d4b6d79147e5826d13
mangalmani-dev <mangalmani2131@gmail.com>
2026-05-07 19:17:58 +0530

    completed with scopes

---
+++ scopes/basics.py
@@ -0,0 +1,48 @@
+# username = "chai aur code"
+
+
+# def func():
+#     # username = "chai"
+#     print(username)
+
+# print(username)
+# func()
+
+
+x = 99
+# def func2(y):
+#     z = x+y
+#     return z
+
+# result = func2(1)
+# print(result)
+
+# def func3():
+#     global x
+#     x = 88
+# func3()
+# print(x)
+
+# climbling
+# def f2():
+#     # x = 88
+#     def f3():
+#         print(x)
+#     return f3
+# myresult = f2()
+# myresult()
+    
+
+# def chaicoder(num):
+#     def actual(x):
+#         return x**num
+#     return actual
+
+# f = chaicoder(2)
+# g = chaicoder(3)
+
+# print(f(3))
+
+
+
+
